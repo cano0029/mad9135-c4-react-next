dc65b2e3e6033a8f75811b5fbe7d47609170029c
Johanna Cano <cano0029@algonquinlive.com>
2021-11-27 18:32:03 -0500

    Add delete pop up before deleting podcast

--- pages/podcasts/[id].js
+++ pages/podcasts/[id].js
@@ -2,14 +2,14 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
 import { AppContext } from '../../Contexts/AppContext';
 import { useContext } from 'react';
 import EditModal from '../../components/EditModal';
 import styles from '../../styles/Details.module.css';
+import Stack from '@mui/material/Stack';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import { Modal, Box, Typography, ButtonUnstyled } from '@mui/material';
 
 export default function PodcastDetails() {
   const router = useRouter();
@@ -17,6 +17,11 @@ export default function PodcastDetails() {
   const [podcast, setPodcast] = useState(null);
   const { podcastHandler } = useContext(AppContext);
   const [showModal, setShowModal] = useState(false);
+
+  //delete pop up
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpenModal = () => setOpenModal(true);
+  const handleCloseModal = () => setOpenModal(false);
 
   useEffect(() => {
     let url = `/api/podcasts/${id}`;
@@ -89,20 +94,16 @@ export default function PodcastDetails() {
                   </div>
                 </div>
                 <Stack className={styles.stack} spacing={2} direction="row">
-                  <Link href="/podcasts" as="/podcasts">
-                    <a>
-                      <DeleteIcon
-                        variant="contained"
-                        onClick={() => {
-                          podcastHandler({ action: 'DELETE', payload: id });
-                        }}
-                        sx={{
-                          marginRight: '5rem',
-                          fontSize: '2rem',
-                        }}
-                      />
-                    </a>
-                  </Link>
+                  <DeleteIcon
+                    variant="contained"
+                    onClick={() => {
+                      handleOpenModal();
+                    }}
+                    sx={{
+                      marginRight: '5rem',
+                      fontSize: '2rem',
+                    }}
+                  />
                   <ModeEditIcon
                     variant="outlined"
                     style={{ fontSize: '2rem' }}
@@ -114,6 +115,40 @@ export default function PodcastDetails() {
               </div>
             </div>
           )}
+          <Modal
+            open={openModal}
+            onClose={handleCloseModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box className={styles.modal}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Are you sure you want to delete this podcast?
+              </Typography>
+              <div>
+                <Link href="/podcasts" as="/podcasts">
+                  <a>
+                    <ButtonUnstyled
+                      style={{ color: 'red', border: '1px solid red' }}
+                      className={styles.button}
+                      onClick={() => {
+                        podcastHandler({ action: 'DELETE', payload: id });
+                      }}
+                    >
+                      Delete
+                    </ButtonUnstyled>
+                  </a>
+                </Link>
+                <ButtonUnstyled
+                  style={{ border: '1px solid grey' }}
+                  className={styles.button}
+                  onClick={() => handleCloseModal()}
+                >
+                  Cancel
+                </ButtonUnstyled>
+              </div>
+            </Box>
+          </Modal>
         </div>
       </div>
     );
